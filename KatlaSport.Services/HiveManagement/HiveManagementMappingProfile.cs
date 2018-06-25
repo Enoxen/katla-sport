96261fe3a991bb7df0a0cde1bd5b5a251f5f160b
Enoxen <kuzmich798@mail.ru>
2018-06-26 01:48:00 +0300

    Add mapping to HiveManagementMappingProfile

--- KatlaSport.Services/HiveManagement/HiveManagementMappingProfile.cs
+++ KatlaSport.Services/HiveManagement/HiveManagementMappingProfile.cs
@@ -13,6 +13,8 @@ namespace KatlaSport.Services.HiveManagement
             CreateMap<DataAccessHive, Hive>();
             CreateMap<DataAccessHiveSection, HiveSectionListItem>();
             CreateMap<DataAccessHiveSection, HiveSection>();
+
+
             CreateMap<UpdateHiveRequest, DataAccessHive>()
                 .ForMember(r => r.LastUpdated, opt => opt.MapFrom(p => DateTime.UtcNow));
         }
