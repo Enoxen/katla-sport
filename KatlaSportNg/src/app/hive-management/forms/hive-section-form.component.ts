58cfafe4fb5b45bbe548bbc39cf538704053ee27
Enoxen <kuzmich798@mail.ru>
2018-06-26 09:54:50 +0300

    added frontend ts and html forms

--- KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HiveSectionService } from 'app/hive-management/services/hive-section.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from 'environments/environment';
+import { HiveSection } from 'app/hive-management/models/hive-section';
+import { HiveService } from 'app/hive-management/services/hive.service';
+import { HiveListItem } from 'app/hive-management/models/hive-list-item';
+import { toInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-hive-section-form',
@@ -7,8 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HiveSectionFormComponent implements OnInit {
 
-  constructor() { }
+  hiveSection = new HiveSection(0, "", "", 0, false, "");
+  hives: HiveListItem[];
+  storeHiveId:number;
+  existed = false;
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private hiveService: HiveService,
+    private hiveSectionService: HiveSectionService
+  ) { }
+ngOnInit(){
+  this.hiveService.getHives().subscribe(hArr => this.hives = hArr);
+    this.route.params.subscribe(p => {
+      this.storeHiveId = toInteger(p['storeHiveId']);
+      if(p['id'] === undefined) {
+        this.hiveSection.storeHiveId = this.storeHiveId;
+      } else {
+        this.hiveSectionService.getHiveSection(p['id']).subscribe(hs => {
+          this.hiveSection = hs;
+          if(isNaN(this.storeHiveId)){
+            this.storeHiveId = hs.storeHiveId;
+          }
+        });
+        this.existed = true;
+      }
+    });
   }
+
+  navigateToHivesSections() {
+       this.router.navigate([`/hive/${this.storeHiveId}/sections`]);
+      }
 }
